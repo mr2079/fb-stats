9263d9db6dd55385e38070ab6a359f63d43b484c
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-12-07 01:48:57 +0330

    Add some basic file & db config

--- fb-stats.server/src/app.module.ts
+++ fb-stats.server/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [ ".env.development", ".env.production" ]
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'fb_stats',
+      models: [],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
