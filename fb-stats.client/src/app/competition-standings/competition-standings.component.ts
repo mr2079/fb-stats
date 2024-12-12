d79f83bde71226ea6c3c639bcaa8c8a75f11475d
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-12-12 19:45:03 +0330

    Competition standing page completed

--- fb-stats.client/src/app/competition-standings/competition-standings.component.ts
+++ fb-stats.client/src/app/competition-standings/competition-standings.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import { BehaviorSubject } from 'rxjs';
+import CompetitionStandingsResponse from './competition-standings.interface';
+import { HttpClient } from '@angular/common/http';
+
+export class PeriodicElement {
+  constructor(
+    public rank: number,
+    public title: string,
+    public score: number,
+    public playedMatches: number,
+    public wonMatches: number,
+    public lostMatches: number,
+    public goalDifference: number
+  ) { }
+}
 
 @Component({
   selector: 'app-competition-standings',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './competition-standings.component.html',
   styleUrl: './competition-standings.component.css'
 })
-export class CompetitionStandingsComponent {
+export class CompetitionStandingsComponent implements OnInit {
+  displayedColumns: string[] = ['rank', 'title', 'playedMatches', 'wonMatches', 'lostMatches', 'goalDifference', 'score'];
+  standingTable = new BehaviorSubject<PeriodicElement[]>([]);
 
+  constructor(
+    private readonly _http: HttpClient
+  ) { }
+
+  @Input() competitionId?: number | null | undefined;
+
+  async ngOnInit(): Promise<void> {
+    if (this.competitionId) {
+      this._http.get<CompetitionStandingsResponse>(`http://localhost:3000/api/v1/competitions/${this.competitionId}/standings`)
+        .subscribe({
+          next: ({standings}) => {
+            this.standingTable.next(
+              standings!.map(s => 
+                new PeriodicElement(
+                  s.rank,
+                  s.team.title,
+                  s.score,
+                  s.playedMatches,
+                  s.wonMatches,
+                  s.lostMatches,
+                  s.goalDifference    
+                )
+              )
+            );
+          }
+        })
+    }
+  }
+
+  // async fetchStandingAsync(competitonId: number): Promise<CompetitionStandingsResponse> {
+  //   return this._http.get<CompetitionStandingsResponse>(`http://localhost:3000/api/v1/competitions/${competitonId}/standings`);
+  // }
 }
--- fb-stats.client/src/app/competition-tabs/competition-tabs.component.ts
+++ fb-stats.client/src/app/competition-tabs/competition-tabs.component.ts
@@ -1,34 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import { BehaviorSubject, first, map, Observable, Observer } from 'rxjs';
 import { MatTabsModule } from '@angular/material/tabs';
 import { AsyncPipe } from '@angular/common';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { HttpClient } from '@angular/common/http';
-import { CompetitionItemsResponse, CompetitionTab } from './competition-tabs.interface';
+import {
+  CompetitionItemsResponse,
+  CompetitionTab,
+} from './competition-tabs.interface';
+import { CompetitionStandingsComponent } from "../competition-standings/competition-standings.component";
 
 @Component({
   selector: 'competition-tabs',
   templateUrl: 'competition-tabs.component.html',
-  imports: [MatTabsModule, AsyncPipe, MatProgressBarModule],
+  imports: [MatTabsModule, AsyncPipe, MatProgressBarModule, CompetitionStandingsComponent],
 })
 export class CompetitionTabsComponent implements OnInit {
-  competitionTabs!: Observable<CompetitionTab[]>;
-  selectedCompetitionId!: Observable<number>;
+  competitionTabs = new BehaviorSubject<CompetitionTab[]>([]);
+  selectedCompetitionId = new BehaviorSubject<number | null | undefined>(null);
 
-  constructor(private readonly _http: HttpClient) { }
+  constructor(private readonly _http: HttpClient) {}
 
   ngOnInit(): void {
-    this._http.get<CompetitionItemsResponse>("http://localhost:3000/api/v1/competitions")
+    this._http
+      .get<CompetitionItemsResponse>(
+        'http://localhost:3000/api/v1/competitions'
+      )
       .subscribe({
-        next: ({competitions})=>{
-          this.competitionTabs = new Observable<CompetitionTab[]>(observer => {
-            observer.next(
-              competitions?.map(item => {
-                return new CompetitionTab(item.id, item.title);
-              })
-            )
-          })
+        next: ({ competitions }) => {
+          this.competitionTabs.next(
+            competitions?.map((item) =>
+               new CompetitionTab(item.id, item.title)) ?? []
+          );
+          this.selectedCompetitionId.next(
+            this.competitionTabs.getValue()[0].id
+          );
         }
-      })
+      });
   }
 }
