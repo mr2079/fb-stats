dfd918ace02b17e5115ca3bf12590b4a5a4b7ca9
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-12-18 00:42:02 +0330

    Add ionic/angular instead angular client

--- fb-stats.client/src/app/app.component.ts
+++ fb-stats.client/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  constructor() {}
 }
